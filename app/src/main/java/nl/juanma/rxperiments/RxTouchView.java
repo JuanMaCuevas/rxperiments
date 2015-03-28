31632bc66013c2c78e2521c48450b0b55dface3b
JuanMa Cuevas <jumacuca@gmail.com>
2015-03-28 02:15:10 +0100

    chain subscribers to draw paths

--- app/src/main/java/nl/juanma/rxperiments/RxTouchView.java
+++ app/src/main/java/nl/juanma/rxperiments/RxTouchView.java
@@ -2,12 +2,19 @@ package nl.juanma.rxperiments;
 
 import android.annotation.TargetApi;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 
 import rx.Observable;
+import rx.functions.Action1;
 import rx.functions.Func1;
 import rx.subjects.PublishSubject;
 
@@ -48,6 +55,8 @@ public class RxTouchView extends View {
                 return true;
             }
         });
+
+        setAfterDragSubscriber();
     }
 
 
@@ -70,6 +79,57 @@ public class RxTouchView extends View {
                     return ev.getActionMasked() == MotionEvent.ACTION_MOVE;
                 }
             });
+
+
+    public void setAfterDragSubscriber() {
+        mDownObservable.subscribe(new Action1<MotionEvent>() {
+            @Override public void call(MotionEvent o) {
+                final Path path = new Path();
+                path.moveTo(o.getX(), o.getY());
+                Log.i("", "down");
+                mMovesObservable
+                        .takeUntil(mUpObservable.doOnNext(new Action1<MotionEvent>() {
+                            @Override public void call(MotionEvent o) {
+                                draw(path);
+                                path.close();
+                                Log.i("", "up");
+                            }
+                        }))
+                        .subscribe(new Action1<MotionEvent>() {
+                            @Override public void call(MotionEvent o) {
+                                path.lineTo(o.getX(), getY());
+                                draw(path);
+                                Log.i("", "move");
+                            }
+                        });
+            }
+        });
+
+    }
+
+    private Bitmap CustomBitmap;
+    private Canvas CustomCanvas;
+    private Paint CustomPaint;
+
+    private void draw(Path path) {
+        if (CustomBitmap == null) {
+            CustomBitmap = Bitmap.createBitmap(getWidth(), getHeight(), Bitmap.Config.ARGB_8888);
+            CustomCanvas = new Canvas(CustomBitmap);
+            CustomPaint = new Paint();
+            CustomPaint.setColor(Color.GREEN);
+            CustomPaint.setStyle(Paint.Style.STROKE);
+        }
+
+        CustomCanvas.drawPath(path, CustomPaint);
+        invalidate();
+    }
+
+
+    public void onDraw(Canvas canvas) {
+        if (CustomBitmap != null)
+            canvas.drawBitmap(CustomBitmap, 0, 0, CustomPaint);
+    }
+
 
 }
 
